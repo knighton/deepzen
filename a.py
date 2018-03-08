038dd0658a53cb93b40f6a3a553e096ce981b270
James Knighton <iamknighton@gmail.com>
2018-03-08 14:49:04 -0800

    Wrap the steps in layers.

--- a.py
+++ a.py
@@ -1,6 +1,34 @@
 import torch
 from torch.autograd import Variable
 
+
+class Layer(object):
+    pass
+
+
+class Dense(Layer):
+    def __init__(self, kernel):
+        self.kernel = kernel
+
+    def forward(self, x):
+        return x.mm(self.kernel)
+
+
+class ReLU(Layer):
+    def forward(self, x):
+        return x.clamp(min=0)
+
+
+class Sequence(Layer):
+    def __init__(self, layers):
+        self.layers = layers
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
+
+
 dtype = torch.FloatTensor
 
 N, D_in, H, D_out = 64, 1000, 100, 10
@@ -11,9 +39,15 @@ y = Variable(torch.randn(N, D_out).type(dtype), requires_grad=False)
 w1 = Variable(torch.randn(D_in, H).type(dtype), requires_grad=True)
 w2 = Variable(torch.randn(H, D_out).type(dtype), requires_grad=True)
 
+model = Sequence([
+    Dense(w1),
+    ReLU(),
+    Dense(w2),
+])
+
 learning_rate = 1e-6
 for t in range(500):
-  y_pred = x.mm(w1).clamp(min=0).mm(w2)
+  y_pred = model.forward(x)
   
   loss = (y_pred - y).pow(2).sum()
   print(t, loss.data[0])
