bf0809d32819620ccf3399a5285a1fc00f2d83a5
James Knighton <iamknighton@gmail.com>
2018-03-08 19:23:07 -0800

    Update softmax API.

--- a.py
+++ a.py
@@ -11,13 +11,7 @@ def dtype_of(x):
 
 
 def softmax(x):
-    x_shape = x.size()
-    tp = x.transpose(1, len(x_shape) - 1)
-    tp_shape = tp.size()
-    input_2d = tp.contiguous().view(-1, tp_shape[-1])
-    _2d = F.softmax(input_2d)
-    nd = _2d.view(*tp_shape)
-    return nd.transpose(1, len(x_shape) - 1)
+    return F.softmax(x, -1)
 
 
 class Form(object):
