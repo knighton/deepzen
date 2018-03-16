14833ed5499c131f6e9e63e95efb07231b73ec4b
James Knighton <iamknighton@gmail.com>
2018-03-15 21:50:39 -0700

    Make Dense layer's bias optional.

--- sunyata/layer/dot/dense.py
+++ sunyata/layer/dot/dense.py
@@ -9,18 +9,25 @@ from ..base.spec import Spec
 class DenseLayer(Layer):
     def __init__(self, kernel, bias):
         self.kernel = Z.variable(kernel)
-        self.bias = Z.variable(bias)
+        if bias is None:
+            self.bias = None
+        else:
+            self.bias = Z.variable(bias)
 
     def params(self):
-        return [self.kernel, self.bias]
+        ret = [self.kernel]
+        if self.bias is not None:
+            ret.append(self.bias)
+        return ret
 
     def forward(self, x, is_training):
         return Z.dense(x, self.kernel, self.bias)
 
 
 class DenseSpec(Spec):
-    def __init__(self, dim=None):
+    def __init__(self, dim=None, has_bias=False):
         self.out_dim = dim
+        self.has_bias = has_bias
 
     def build(self, form=None):
         in_dim, = form.shape
@@ -30,6 +37,9 @@ class DenseSpec(Spec):
             out_dim = self.out_dim
         kernel = np.random.normal(
             0, 0.1, (out_dim, in_dim)).astype('float32')
-        bias = np.zeros(out_dim, 'float32')
+        if self.has_bias:
+            bias = np.zeros(out_dim, 'float32')
+        else:
+            bias = None
         out_shape = (out_dim,)
         return DenseLayer(kernel, bias), Form(out_shape, form.dtype)
