2f091f5abaed65e5c123d55cb7540721c9de8092
James Knighton <iamknighton@gmail.com>
2018-03-22 04:20:01 -0700

    Fix (import).

--- deepzen/api/pytorch/transform/__init__.py
+++ deepzen/api/pytorch/transform/__init__.py
@@ -1,7 +1,9 @@
 from ...base.transform import BaseTransformAPI
 from .activ import PyTorchActivAPI
+from .arch import PyTorchArchAPI
 from .dot import PyTorchDotAPI
 
 
-class PyTorchTransformAPI(BaseTransformAPI, PyTorchActivAPI, PyTorchDotAPI):
+class PyTorchTransformAPI(BaseTransformAPI, PyTorchActivAPI, PyTorchArchAPI,
+                          PyTorchDotAPI):
     pass
